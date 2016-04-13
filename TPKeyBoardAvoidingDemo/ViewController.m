a533c6f3c715f312796ec2dca5f1acb37a09636e
UILable <shoupeng.ji@urgoo.cn>
2016-04-13 13:42:48 +0800

    TPKeyBoardAvoidingDemo
    TPKeyBoardAvoidingDemo

---
+++ TPKeyBoardAvoidingDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TPKeyBoardAvoidingDemo
+//
+//  Created by admin on 16/4/13.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+
+//TPKeyboardAvoiding的使用
+//只能在Xib中使用，自适应高度
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
